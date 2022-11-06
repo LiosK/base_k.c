2f82ff8f6c813c8e85f61743cea246254acb30de
LiosK <contact@mail.liosk.net>
2022-11-06 09:31:51 +0900

    perf: pin word size of base_k_convert_radix() across loops

--- base_k.h
+++ base_k.h
@@ -25,14 +25,17 @@
 extern "C" {
 #endif
 
-#ifdef BASE_K_USE_NAIVE
 /** Converts a digit value array in `src_radix` to that in `dst_radix`. */
 int base_k_convert_radix(uint8_t *dst, const uint8_t *src, int dst_radix,
                          int src_radix, int dst_size, int src_size) {
+  assert(2 <= dst_radix && dst_radix <= 256 && dst_size >= 0);
+  assert(2 <= src_radix && src_radix <= 256 && src_size >= 0);
+
   for (int i = 0; i < dst_size; i++) {
     dst[i] = 0;
   }
 
+#ifdef BASE_K_USE_NAIVE
   for (int i = 0; i < src_size; i++) {
     uint_fast32_t carry = src[i];
     for (int j = dst_size - 1; j >= 0; j--) {
@@ -44,24 +47,27 @@ int base_k_convert_radix(uint8_t *dst, const uint8_t *src, int dst_radix,
       return -1; // dst_size too small
     }
   }
-  return 0;
-}
 #else
-/** Converts a digit value array in `src_radix` to that in `dst_radix`. */
-int base_k_convert_radix(uint8_t *dst, const uint8_t *src, int dst_radix,
-                         int src_radix, int dst_size, int src_size) {
-  for (int i = 0; i < dst_size; i++) {
-    dst[i] = 0;
+  // Determine optimum number of digits to read per loop
+  int word_size = 0;
+  uint64_t word_radix = 1;
+  while (word_radix < UINT64_MAX / ((uint64_t)src_radix * dst_radix)) {
+    word_size++;
+    word_radix *= src_radix;
   }
 
-  int dst_used = dst_size - 1;
-  uint64_t carry = 0;
-  for (int i = 0; i < src_size;) {
-    // Reset carry to input (read multiple digits for optimization)
-    uint64_t power = 1; // Set to src_radix ^ number of digits read
-    while (power < ((uint64_t)1 << 48) && i < src_size) {
-      carry = carry * src_radix + src[i++];
-      power *= src_radix;
+  int dst_used = dst_size - 1; // storage to memorize dst range already filled
+
+  // Align initial src cursor so it points to heads of words in loop
+  int i = src_size % word_size;
+  if (i > 0) {
+    i -= word_size;
+  }
+  for (; i < src_size; i += word_size) {
+    // Read word_size digits from src to carry
+    uint64_t carry = 0;
+    for (int j = i < 0 ? 0 : i; j < i + word_size; j++) {
+      carry = carry * src_radix + src[j];
     }
 
     // Iterate over dst from right while carry != 0 or up to place already used
@@ -70,16 +76,17 @@ int base_k_convert_radix(uint8_t *dst, const uint8_t *src, int dst_radix,
       if (j < 0) {
         return -1; // dst_size too small
       }
-      carry += dst[j] * power;
+      carry += dst[j] * word_radix;
       dst[j] = carry % dst_radix;
       carry = carry / dst_radix;
     }
     dst_used = j + 1;
     assert(carry == 0 && dst_used >= 0);
   }
-  return 0;
-}
 #endif /* #ifdef BASE_K_USE_NAIVE */
+
+  return 0; // success
+}
 
 #ifdef __cplusplus
 } /* extern "C" { */
